2c8e057d857eaaef7aa9a2cab4dc04e380cba026
KSG <tjdzms123@naver.com>
2024-05-07 18:01:15 +0900

    서버 연결 중 오류(cors)

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -8,45 +8,26 @@ import {
 } from "@mui/material";
 import Customer from "./components/Customer";
 import { styled } from "@mui/material/styles";
-
-const StyledPaper = styled(Paper)({
-  width: "100%",
-  marginTop: (theme) => theme.spacing(3),
-  overflowX: "auto",
-});
-
-const StyledTable = styled(Table)({
-  minWidth: 1080,
-});
-
-const customers = [
-  {
-    id: "1",
-    image: "https://picsum.photos/64",
-    name: "홍길동",
-    birthday: "000101",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: "2",
-    image: "https://picsum.photos/64",
-    name: "김길동",
-    birthday: "000202",
-    gender: "남자",
-    job: "백수",
-  },
-  {
-    id: "3",
-    image: "https://picsum.photos/64",
-    name: "이길동",
-    birthday: "000303",
-    gender: "여자",
-    job: "간호사",
-  },
-];
+import { useEffect, useState } from "react";
+import api from './axios/api'
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    getCustomers();
+  }, []);
+
+  const getCustomers = async () => {
+    try {
+      const res = await api.get("/api/customers");
+      // setCustomers(res);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <StyledPaper>
       <StyledTable>
@@ -71,3 +52,13 @@ function App() {
 }
 
 export default App;
+
+const StyledPaper = styled(Paper)({
+  width: "100%",
+  marginTop: (theme) => theme.spacing(3),
+  overflowX: "auto",
+});
+
+const StyledTable = styled(Table)({
+  minWidth: 1080,
+});
--- management/server.js
+++ management/server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: "1",
+      image: "https://picsum.photos/64",
+      name: "홍길동",
+      birthday: "000101",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: "2",
+      image: "https://picsum.photos/64",
+      name: "김길동",
+      birthday: "000202",
+      gender: "남자",
+      job: "백수",
+    },
+    {
+      id: "3",
+      image: "https://picsum.photos/64",
+      name: "이길동",
+      birthday: "000303",
+      gender: "여자",
+      job: "간호사",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
