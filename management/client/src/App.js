292bda2b940391affea05476d70884c5a9ecff8d
KSG <tjdzms123@naver.com>
2024-05-08 13:14:32 +0900

    MUI 문법 최신화 및 데이터 로딩 시 처리 기능 추가

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -5,14 +5,16 @@ import {
   TableHead,
   TableRow,
   Paper,
+  CircularProgress,
 } from "@mui/material";
 import Customer from "./components/Customer";
-import { styled } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import api from "./axios/api";
+import { styled } from "@mui/system";
 
-function App() {
+function App(props) {
   const [customers, setCustomers] = useState([]);
+  const [completed, setCompleted] = useState(0);
 
   useEffect(() => {
     getCustomers();
@@ -24,6 +26,18 @@ function App() {
       setCustomers(data);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    setInterval(progress(), 20);
+  }, []);
+
+  const progress = () => {
+    if (completed >= 100) {
+      setCompleted(0);
+    } else {
+      setCompleted(completed + 1);
     }
   };
 
@@ -41,9 +55,13 @@ function App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers.map((c) => (
-            <Customer key={c.id} customers={c} />
-          ))}
+          {customers?.length > 0 ? (
+            customers?.map((c) => <Customer key={c.id} customers={c} />)
+          ) : (
+            <TableRow variant="determinate">
+              <StyledProgress value={completed} />
+            </TableRow>
+          )}
         </TableBody>
       </StyledTable>
     </StyledPaper>
@@ -52,12 +70,16 @@ function App() {
 
 export default App;
 
-const StyledPaper = styled(Paper)({
+const StyledPaper = styled(Paper)(({ theme }) => ({
   width: "100%",
-  marginTop: (theme) => theme.spacing(3),
+  marginTop: theme.spacing(3),
   overflowX: "auto",
-});
+}));
 
 const StyledTable = styled(Table)({
   minWidth: 1080,
 });
+
+const StyledProgress = styled(CircularProgress)(({ theme }) => ({
+  margin: theme.spacing(3),
+}));
